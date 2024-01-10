d364f9fc48ab16c9a2d9f4fde9e9dfa192f0f9a8
Guanqun Yang <guanqun.yang@outlook.com>
2024-01-10 17:55:50 -0500

    Update setting.py

--- setting/setting.py
+++ setting/setting.py
@@ -2,7 +2,7 @@ import pathlib
 
 # base_path is only used for local debugging purposes
 base_path = pathlib.Path().absolute().parent
-debug = True
+debug = False
 
 # parameters to control the behavior of the recommendation system
 topk = 125
